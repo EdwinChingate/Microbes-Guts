cf5a5638db07f3393eff5c1456ed3d2e7d2327f7
eachingateb <eachingateb@unal.edu.co>
2024-02-26 17:39:27 -0500

    New control panel with just one slot for steps

--- Functions/Geometry.R
+++ Functions/Geometry.R
@@ -1,7 +1,11 @@
 Geometry <- function(reactor_id,Parameters){
-  GridSize <- round(as.numeric(Parameters[4*reactor_id,2]))
-  Width <- as.numeric(Parameters[4*reactor_id + 1,2])
-  steps <- round(as.numeric(Parameters[4*reactor_id + 2,2]))
-  Length <- as.numeric(Parameters[4*reactor_id + 3,2])
+  TotalLength <- sum(as.numeric(c(Parameters[7,2],Parameters[10,2],Parameters[13,2],Parameters[16,2])))
+  AllSteps <- as.numeric(Parameters[4,2])
+  GridSize <- round(as.numeric(Parameters[3*reactor_id+2,2]))
+  Width <- as.numeric(Parameters[3*reactor_id + 3,2])
+  #steps <- round(as.numeric(Parameters[4*reactor_id + 2,2])) #Update with the TotalLenght and the total steps
+  Length <- as.numeric(Parameters[3*reactor_id + 4,2])
+  LenghtFraction <- Length/TotalLength
+  steps <- LenghtFraction*AllSteps
   return( as.numeric(c(GridSize,Width,steps,Length)))
 }
