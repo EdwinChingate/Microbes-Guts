abe7eda7e1b8762cfa386667549137fc11b1b30b
eachingateb <eachingateb@unal.edu.co>
2024-04-25 10:00:00 -0500

    Not eval

--- Microbes-Guts-Script.R
+++ Microbes-Guts-Script.R
@@ -1,7 +1,7 @@
 closeAllConnections()
 rm(list=ls()) #to clean memory, better to always run it
 
-home <- "~/LRZ Sync+Share/GitHubProjects/Codding/Microbes-Guts" #getwd()
+home <- "/home/edwin/0-GitHubProjects/Codding/Microbes-Guts-Simulator" #getwd()
 setwd(home)
 source('Functions/Guts.R')
 eval <- Guts(home,Parameters_folder='ParametersRun',Models_folder='MicroModelsRun',SaveResults=TRUE) #Default values Parameters_folder='Parameters',Models_folder='MicroModel'
