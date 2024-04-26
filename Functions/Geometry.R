3bc48668885638746eba92c4fae5e11883f7ed68
eachingateb <eachingateb@unal.edu.co>
2024-04-25 21:10:00 -0500

    Geometry for all steps

--- Functions/Geometry.R
+++ Functions/Geometry.R
@@ -1,10 +1,7 @@
 Geometry <- function(reactor_id,Parameters){
-  TotalLength <- sum(as.numeric(c(Parameters[7,2],Parameters[10,2],Parameters[13,2],Parameters[16,2])))
-  AllSteps <- as.numeric(Parameters[4,2])
-  GridSize <- round(as.numeric(Parameters[3*reactor_id+2,2]))
-  Width <- as.numeric(Parameters[3*reactor_id + 3,2])
-  Length <- as.numeric(Parameters[3*reactor_id + 4,2])
-  LenghtFraction <- Length/TotalLength
-  steps <- as.integer(LenghtFraction*AllSteps+1)
+  GridSize <- round(as.numeric(Parameters[4*reactor_id,2]))
+  Width <- as.numeric(Parameters[4*reactor_id + 1,2])
+  steps <- round(as.numeric(Parameters[4*reactor_id + 2,2]))
+  Length <- as.numeric(Parameters[4*reactor_id + 3,2])
   return( as.numeric(c(GridSize,Width,steps,Length)))
 }
